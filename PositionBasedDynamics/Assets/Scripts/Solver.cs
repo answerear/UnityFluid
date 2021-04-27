d5810745cbf8a466d317b21c911690b4cb7e2aa5
aaronwang <aaronwang@tencent.com>
2021-04-27 20:14:49 +0800

    Modified   PositionBasedDynamics/Assets/Scripts/Solver.cs Modified   doc/基于物理的流体渲染——从理论到实践.md Added      doc/image/PBD-lagrange-multiplier-shortest-path.jpg

--- PositionBasedDynamics/Assets/Scripts/Solver.cs
+++ PositionBasedDynamics/Assets/Scripts/Solver.cs
@@ -13,6 +13,16 @@ namespace PositionBasedDynamics
         public int numIterations { get; set; }
 
         /// <summary>
+        /// 重力加速度
+        /// </summary>
+        public Vector3 gravity { get; set; }
+
+        /// <summary>
+        /// 最大交互半径
+        /// </summary>
+        public float radius { get; set; }
+
+        /// <summary>
         /// 非流体粒子要保持的距离，必须在 (0, radius] 范围内
         /// </summary>
         public float solidRestDistance { get; set; }
@@ -24,6 +34,12 @@ namespace PositionBasedDynamics
         /// </summary>
         public float fluidRestDistance { get; set; }
 
+        /// <summary>
+        /// 松弛因子，控制并行求解器的收敛速度，默认值为：1，大于1的值可能会导致不稳定
+        /// </summary>
+        public float relaxationFactor { get; set; }
+
+        #region common
         /// <summary>
         /// 与有形状物体碰撞时使用的摩擦系数 
         /// </summary>
@@ -44,11 +60,53 @@ namespace PositionBasedDynamics
         /// </summary>
         public float restitution { get; set; }
 
+
         /// <summary>
-        /// 
+        /// 附着力。 控制粒子在其撞击的表面上的附着的强度，默认为 0.0 ，范围为 [0.0, +∞]
         /// </summary>
         public float adhesion { get; set; }
 
+        /// <summary>
+        /// 速度阈值。 当速度值小于阈值时，使用固定速度值
+        /// </summary>
+        public float sleepThreshold { get; set; }
+
+        /// <summary>
+        /// 最大速度。 粒子速度超过该值时，直接被 clamp 在该值上
+        /// </summary>
+        public float maxSpeed { get; set; }
+
+        /// <summary>
+        /// 最大加速度。 粒子加速度超过该值时，直接被 clamp 在该值上。 这避免由于大的互穿透而引起的震荡
+        /// </summary>
+        public float maxAcceleration { get; set; }
+
+        /// <summary>
+        /// 基于固定参考点高度上，人为减少粒子质量，这使得 stack 和 pile 更快收敛
+        /// </summary>
+        public float shockPropagation { get; set; }
+
+        /// <summary>
+        /// 损耗，基于粒子接触的数量来降低粒子速度
+        /// </summary>
+        public float dissipation { get; set; }
+
+        /// <summary>
+        /// 阻尼，粘性阻力，施加与速度成反比的力
+        /// </summary>
+        public float damping { get; set; }
+
+        #endregion
+
+        #region 布料
+        public Vector3 wind { get; set; }
+
+        public float drag { get; set; }
+
+        public float lift { get; set; }
+        #endregion
+
+        #region 流体
         public float cohesion { get; set; }
 
         public float surfaceTension { get; set; }
@@ -57,17 +115,49 @@ namespace PositionBasedDynamics
 
         public float viscosityConfinement { get; set; }
 
-        public void AddForce(Force force)
-        {
+        public float anisotropyScale { get; set; }
 
-        }
+        public float anisotropyMin { get; set; }
+
+        public float anisotropyMax { get; set; }
+
+        public float smoothing { get; set; }
+
+        public float solidPressure { get; set; }
+
+        public float freeSurfaceDrag { get; set; }
+
+        public float buoyancy { get; set; }
+        #endregion
+
+        #region 散射
+        public float diffuseThreshold { get; set; }
+
+        public float diffuseBuoyancy { get; set; }
+
+        public float diffuseDrag { get; set; }
+
+        public float diffuseLifetime { get; set; }
+        #endregion
+
+        #region 碰撞
+        public float collisionDistance { get; set; }
+
+        public float particleCollisionMargin { get; set; }
+
+        public float shapeCollisionMargin { get; set; }
+
+        public float [,] planes { get; set; }
+
+        public int numPlanes;
+        #endregion
 
         public void AddEntity(Entity entity)
         {
 
         }
 
-        public void Step(float dt, int substeps)
+        public void Step(float dt)
         {
 
         }
